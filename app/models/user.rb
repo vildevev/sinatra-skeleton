f52071081bb068fe6a72cdc44698308405bfe06e
Vilde Vevatne <vilde@vevatne.no>
2017-04-16 16:46:58 -0700

    Adds validations and BCrypt to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+  validates :email, uniqueness: true
+  validates :email, :password_hash, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+  
 end
