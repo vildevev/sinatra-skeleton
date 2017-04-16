4d50e4753db9b1d387446bc3932f862a12d6e89a
Vilde Vevatne <vilde@vevatne.no>
2017-04-16 16:55:03 -0700

    Adds post route for creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 get '/users/new' do
-  erb :'users/new'
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    login
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
 end
