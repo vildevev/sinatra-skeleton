98d841426b234ecb74abf36c8a9d4bc5305ded5e
Vilde Vevatne <vilde@vevatne.no>
2017-04-16 17:07:08 -0700

    Adds post route in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 get '/sessions/new' do
   erb '/sessions/new'
 end
+
+post '/sessions' do
+  @user = User.find_by(email: params[:user][:email])
+  if @user && @user.authenticate(params[:user][:password])
+    login
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["Please input valid user information"]
+    erb :'/sessions/new'
+  end
+end
